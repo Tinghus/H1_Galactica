31e38850ecd2f915efa4f47fea48fd90bb7eefca
Tinghus <m.tinghus@gmail.com>
2023-03-09 13:56:49 +0100

    Added comments

--- H1_Galactica/ExtraHardcore.cs
+++ H1_Galactica/ExtraHardcore.cs
@@ -13,7 +13,11 @@ namespace H1_Galactica
         {
             CoordSet endingCoords = new CoordSet();
 
+            // We need to take into account that we can have more than 1 revolution in our timespan. So we use modulo to get the remainder
+            // We then use the remainder to calculate our radian, 1 radian = 57 degrees
             double angle = 2 * Math.PI * (timeSpanInYears % (RevolutionPeriodInDays / 365.0)) / (RevolutionPeriodInDays / 365.0);
+
+            // We use our angle with our cosine and sine to calculate the position of x and y
             double x = startingCoords.xCoord * Math.Cos(angle) - startingCoords.yCoord * Math.Sin(angle);
             double y = startingCoords.xCoord * Math.Sin(angle) + startingCoords.yCoord * Math.Cos(angle);
 
